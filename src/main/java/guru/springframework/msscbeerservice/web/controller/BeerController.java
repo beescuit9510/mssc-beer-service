22dfdcae99085334ec21a1d973538c7d0fb19ba8
beescuit9510 <pengsoon20@gmail.com>
2022-03-13 17:12:39 +0900

    adding beer controller closes #3

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("{beerId}")
+     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+        //todo impl
+
+        ResponseEntity<BeerDto> response = new ResponseEntity<>(HttpStatus.OK);
+
+        return response;
+    };
+
+    @PostMapping
+    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto){
+        //todo impl
+
+        ResponseEntity<BeerDto> response = new ResponseEntity<>(HttpStatus.OK);
+
+        return response;
+
+    };
+
+    @PutMapping("{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody  BeerDto beerDto){
+        //todo impl
+
+        ResponseEntity response = new ResponseEntity<>(HttpStatus.OK);
+
+        return response;
+
+    };
+}
